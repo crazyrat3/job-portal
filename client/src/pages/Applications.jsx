77b2c812b6e9c0c99e77284ed8ad0a1b6cd3789e
crazyrat3 <theclockman0304@gmail.com>
2025-11-06 13:45:23 +0530

    corrected imports

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import Navbar from '../components/NavBar'
+import Navbar from '../components/Navbar'
 import { assets, jobsApplied } from '../assets/assets'
 import moment from 'moment'
 import Footer from '../components/Footer'
